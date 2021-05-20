fd131f15ff0bbbe3e3e2d8e29363483388df35f9
Ankush Garg <f20201732@goa.bits-pilani.ac.in>
2021-05-20 17:43:09 +0530

    day6goingon

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String username = "";
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -8,15 +14,13 @@ class LoginPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(
-                height: 42,
-              ),
               Image.asset("assets/images/login_car.png", fit: BoxFit.cover),
               SizedBox(
                 height: 20,
               ),
               Text(
-                "WELCOME",
+                "Welcome $username",
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 36,
                   fontWeight: FontWeight.bold,
@@ -34,10 +38,16 @@ class LoginPage extends StatelessWidget {
                           fontSize: 15,
                         ),
                       ),
+                      onChanged: (value) {
+                        username = value;
+                        setState(() {});
+                      },
                     ),
+
                     SizedBox(
                       height: 15,
                     ),
+
                     TextFormField(
                       obscureText: true,
                       decoration: InputDecoration(
@@ -47,22 +57,46 @@ class LoginPage extends StatelessWidget {
                         ),
                       ),
                     ),
+
                     SizedBox(
-                      height: 60,
+                      height: 45,
                     ),
-                    ElevatedButton(
-                      child: (Text(
-                        "SIGN  IN",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      )),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      onPressed: () {
+
+                    InkWell(
+                      onTap: () {
                         Navigator.pushNamed(context, "/home");
                       },
-                    ),
+                      child: Container(
+                        width: 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    )
+
+                    // ElevatedButton(
+                    //   child: (Text(
+                    //     "SIGN  IN",
+                    //     style: TextStyle(
+                    //       fontWeight: FontWeight.bold,
+                    //       fontSize: 18,
+                    //     ),
+                    //   )),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, "/home");
+                    //   },
+                    // ),
                   ],
                 ),
               )
